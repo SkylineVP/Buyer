9e339747b2661f6c5474491607f3d7d1bc5d4b66
Vladislav Polishchuk <vladpolizp@gmail.com>
2024-10-03 12:46:38 +0300

    UI components + prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: parseInt(process.env.VITE_APP_PORT ??  "3000"),
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })
