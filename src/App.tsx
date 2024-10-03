e46c78272eb6f57712e6011265e36831d25ec4f5
Vladislav Polishchuk <vladpolizp@gmail.com>
2024-10-03 20:52:26 +0300

    prettier lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import {Button} from "@/components/ui/button.tsx";
+import "./App.css";
+import { Button } from "@/components/ui/button.tsx";
 import ThemeToggle from "@/components/ThemeToggle.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className=' bg-background flex items-center justify-center h-[100vh]'>
-          <Button onClick={()=>setCount(count+1)}>Hello + {count}</Button>
-        <ThemeToggle className="absolute top-1 right-1"/>
+    <div className=" bg-background flex items-center justify-center h-[100vh]">
+      <Button onClick={() => setCount(count + 1)}>Hello + {count}</Button>
+      <ThemeToggle className="absolute top-1 right-1" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {ThemeProvider} from "@/components/providers/ThemeProvider.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "@/components/providers/ThemeProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <App />
-      </ThemeProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
